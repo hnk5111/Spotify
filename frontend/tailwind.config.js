45395e9858c00706dd91f94cd35de7ca06772979
rohittt37 <rohitttgupta808@gmail.com>
2025-02-17 19:09:44 +0530

    ad

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     darkMode: ["class"],
     content: [
         './pages/**/*.{ts,tsx}',
@@ -132,5 +132,5 @@ module.exports = {
     		}
     	}
     },
-	plugins:[require("tailwindcss-animate")],
+	plugins:[await import("tailwindcss-animate")],
 };
